9070573ac76b563f300632d57eeecba4d7ebaf27
Marcus Bass <Bass.Marcus@icloud.com>
2019-07-09 06:51:13 -0700

    html boiler

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <form action="" method="POST">
+        <label>Name</label>
+        <input type="text" name="name" value="Enter your name">
+        <label>Location</label>
+        <input type="text" name="location" calue="Enter your location">
+        <button type="submit" name="save">Save</button>
+        <script src="" async defer></script>
+    </body>
+</html>
